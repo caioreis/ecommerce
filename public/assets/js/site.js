831c4e30097cd69fc90e7f521609a629cd88bce5
caioreis <caio@cyclumsn.com>
2014-08-03 02:12:34 -0300

    Cleaning

--- public/assets/js/site.js
+++ public/assets/js/site.js
@@ -12,16 +12,17 @@ $(document).ready(function () {
 				navigation : true,
 				slideSpeed : 300,
 				paginationSpeed : 400,
-				itemsCustom : [
-				[0, 1],
-				[450, 1],
-				[600, 1],
-				[700, 2],
-				[1000, 4],
-				[1200, 4],
-				[1400, 4],
-				[1600, 4]
-				],
+				items: 4,
+				// itemsCustom : [
+				// [0, 1],
+				// [450, 1],
+				// [600, 1],
+				// [700, 2],
+				// [1000, 4],
+				// [1200, 4],
+				// [1400, 4],
+				// [1600, 4]
+				// ],
 				navigationText: ["", ""]
 			});
 		}
